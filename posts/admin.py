19386bb7e9e48a58dbcae42de4bf077ca9105bed
Gulftown7 <interuros@gmail.com>
2017-09-03 11:34:04 +0200

    first view

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from posts.models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "timestamp","update"]
+    list_display_links = ["title", "timestamp"]
+    list_filter = ["title", "timestamp"]
+    search_fields = ["title", "content"]
+    class Meta:
+        model = Post
+        
+    
+
+admin.site.register(Post, PostAdmin)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+
 
 # Create your views here.
+
+def post_home(requests):
+    return HttpResponse("<h1>Gulftown</h1>")
+
